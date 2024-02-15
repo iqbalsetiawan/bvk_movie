bd8de794bf45c383a71bdcb8264405d655a73949
Iqbal Setiawan <ibal.setiawan08@gmail.com>
2024-02-15 19:55:10 +0700

    set header as sticky

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Logo from "../assets/Logo.png";
-import { useOnlineStatus } from '../context/OnlineStatusContext';
+import { useOnlineStatus } from "../context/OnlineStatusContext";
 
 function Header({ onSearch, children }) {
   const [searchTerm, setSearchTerm] = useState("");
@@ -22,43 +22,41 @@ function Header({ onSearch, children }) {
 
   return (
     <>
-      <div>
-        <header className="header">
-          <img
-            src={Logo}
-            alt="Logo"
-            className="logo"
+      <header className="header">
+        <img
+          src={Logo}
+          alt="Logo"
+          className="logo"
+          onClick={() => {
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+            navigate("/", { replace: true });
+          }}
+        />
+        <div className="right-header">
+          <button
+            className="header-watched-button"
             onClick={() => {
               window.scrollTo({
                 top: 0,
                 behavior: "smooth",
               });
-              navigate("/");
+              navigate("/watched");
             }}
+          >
+            Watched Movies
+          </button>
+          <input
+            type="text"
+            placeholder="Search for movies..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            disabled={!isOnline}
           />
-          <div className="right-header">
-            <button
-              className="header-watched-button"
-              onClick={() => {
-                window.scrollTo({
-                  top: 0,
-                  behavior: "smooth",
-                });
-                navigate("/watched");
-              }}
-            >
-              Watched Movies
-            </button>
-            <input
-              type="text"
-              placeholder="Search for movies..."
-              value={searchTerm}
-              onChange={handleSearchChange}
-              disabled={!isOnline}
-            />
-          </div>
-        </header>
-      </div>
+        </div>
+      </header>
       <div>{children}</div>
     </>
   );
